05337fa27e94f72f811fe300287794a11f564fc8
gopshyam <gopal.shyam94@gmail.com>
2016-10-18 05:25:06 -0400

    okay, THIS time it's done

--- id3_prune.py
+++ id3_prune.py
@@ -1,24 +1,29 @@
 import pickle
 
 from id3_predict import find_error
+from id3_create import split_data
 
-root_node = None
-
-def find_optimal_node(current_node, data, feature_labels, root_node):
+def find_optimal_node(current_node, data, feature_labels):
     if (current_node.is_pure):
         return
 
+    if len(data) == 0:
+        return
+
+    split_feature_index = feature_labels.index(current_node.feature_label)
+    neg_set, pos_set = split_data(data, split_feature_index)
+
     if (current_node.left_node != None):
-        find_optimal_node(current_node.left_node, data, feature_labels, root_node)
+        find_optimal_node(current_node.left_node, neg_set, feature_labels)
 
     if (current_node.right_node != None):
-        find_optimal_node(current_node.right_node, data, feature_labels, root_node)
+        find_optimal_node(current_node.right_node, pos_set, feature_labels)
 
-    current_error = find_error(data, root_node, feature_labels)
+    current_error = find_error(data, current_node, feature_labels)
 
     current_node.is_pure = True
 
-    new_error = find_error(data, root_node, feature_labels)
+    new_error = find_error(data, current_node, feature_labels)
 
     if (new_error <= current_error):
         return
@@ -38,7 +43,7 @@ def main(n):
 
     data = [x.strip().split(',') for x in raw_data[1:]]
     
-    find_optimal_node(root_node, data, feature_labels, root_node)
+    find_optimal_node(root_node, data, feature_labels)
 
     with open('pruned_root_node' + str(n), 'w') as f: 
         pickle.dump(root_node, f)
